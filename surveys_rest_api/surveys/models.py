52bb3446675bb5821c4a21fcd23b9daab47fa0bb
Javier Adriel <reivaj0705@gmail.com>
2016-03-04 18:01:49 +0000

    Creat survey model

--- surveys_rest_api/surveys/models.py
+++ surveys_rest_api/surveys/models.py
@@ -2,4 +2,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class Survey(models.Model):
+
+    class Meta:
+        verbose_name = "Survey"
+        verbose_name_plural = "Surveys"
+
+    title = models.CharField(max_length=80)
+    description = models.TextField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
