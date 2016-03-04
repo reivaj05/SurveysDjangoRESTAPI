171c9468979aa81649582e0ed60921c38bf6c735
Javier Adriel <reivaj0705@gmail.com>
2016-03-04 19:26:01 +0000

    Add accounts app to settings base file

--- surveys_rest_api/config/settings/base.py
+++ surveys_rest_api/config/settings/base.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'surveys.apps.SurveysConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE_CLASSES = [
