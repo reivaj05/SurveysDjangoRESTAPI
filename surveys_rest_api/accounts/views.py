adc4975f9df41782e4c463be1873a716befd27ad
Javier Adriel <reivaj0705@gmail.com>
2016-03-07 18:48:22 +0000

    Add user profile view set

--- surveys_rest_api/accounts/views.py
+++ surveys_rest_api/accounts/views.py
@@ -1,24 +1,8 @@
-from django.views.generic import DetailView
-from common.mixins import LoginRequiredMixin
+from rest_framework import viewsets
 from .models import UserProfile
-# Create your views here.
+from .serializers import UserProfileSerializer
 
 
-class UserProfileDetailView(LoginRequiredMixin, DetailView):
-        template_name = 'account/user_profile.html'
-        context_object_name = 'user_profile'
-
-        def get_object(self, queryset=None):
-            authentication_user = self.request.user
-            user, created = UserProfile.objects.get_or_create(
-                authentication_user=authentication_user
-            )
-            return user
-
-        def get_context_data(self, **kwargs):
-            context = super(UserProfileDetailView, self).get_context_data(
-                **kwargs
-            )
-            user = self.request.user
-            context['user_accounts'] = user.socialaccount_set.all()
-            return context
+class UserProfileViewSet(viewsets.ModelViewSet):
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
