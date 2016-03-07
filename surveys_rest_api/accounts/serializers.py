c7b4faca99415353518a20230f7afe52c575db58
Javier Adriel <reivaj0705@gmail.com>
2016-03-07 18:14:17 +0000

    Add serializer for user profile model

---
+++ surveys_rest_api/accounts/serializers.py
@@ -0,0 +1,45 @@
+# from django.contrib.auth import update_session_auth_hash
+from rest_framework import serializers
+from .models import UserProfile
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    # password = serializers.CharField(write_only=True, required=False)
+    # confirm_password = serializers.CharField(write_only=True, required=False)
+
+    class Meta:
+        model = UserProfile
+        fields = (
+            'id', 'email', 'username', 'date_joined',
+            'updated_at', 'first_name', 'last_name',
+            'birthday', 'gender', 'id_network',
+            'biography', 'image_profile'
+            '',
+            # 'password', 'confirm_password',
+        )
+
+        read_only_fields = ('date_joined', 'updated_at')
+
+        def create(self, validated_data):
+            return UserProfile.objects.create(**validated_data)
+
+        def update(self, instance, validated_data):
+            instance.username = validated_data.get(
+                'username', instance.username
+            )
+            instance.tagline = validated_data.get(
+                'tagline', instance.tagline
+            )
+
+            instance.save()
+
+            # password = validated_data.get('password', None)
+            # confirm_password = validated_data.get('confirm_password', None)
+
+            # if password and confirm_password and password==confirm_password:
+            #     instance.set_password(password)
+            #     instance.save()
+
+            # update_session_auth_hash(self.context.get('request'), instance)
+
+            return instance
