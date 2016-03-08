8c43885f010417df4fec5313a96c019c8cdf8337
Javier Adriel <reivaj0705@gmail.com>
2016-03-08 04:02:43 +0000

    Override create method to save section nested serializer

--- surveys_rest_api/surveys/serializers.py
+++ surveys_rest_api/surveys/serializers.py
@@ -1,14 +1,38 @@
 # from django.contrib.auth import update_session_auth_hash
 from rest_framework import serializers
-from .models import Survey
+from .models import Section, Survey
+
+
+class SectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Section
+        fields = (
+            'id', 'title', 'description',
+        )
 
 
 class SurveySerializer(serializers.ModelSerializer):
+    sections = SectionSerializer(many=True)
+
     class Meta:
         model = Survey
         fields = (
             'id', 'title', 'description', 'created_at',
-            'updated_at', 'active',
+            'updated_at', 'active', 'sections', 'user'
         )
 
-        read_only_fields = ('created_at', 'updated_at', 'active')
+        read_only_fields = ('created_at', 'updated_at', 'active', 'user')
+
+    def create(self, validated_data):
+        sections_data = validated_data.pop('sections')
+        survey = Survey.objects.create(**validated_data)
+        for section in sections_data:
+            Section.objects.create(survey=survey, **section)
+
+        # self.create_data(sections_data, Section, 'survey=fk', survey)
+        return survey
+
+    def create_data(self, data, model, assignment, fk):
+        print eval(assignment)
+        for element in data:
+            model.objects.create(eval(assignment), **element)
