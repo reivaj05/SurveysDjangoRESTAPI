1ebf50e81ab807e3f5f74af56491573c36f8042e
Javier Adriel <reivaj0705@gmail.com>
2016-03-08 02:16:50 +0000

    Add permission IsOwnerOrReadOnly

--- surveys_rest_api/accounts/views.py
+++ surveys_rest_api/accounts/views.py
@@ -1,10 +1,14 @@
 from rest_framework import permissions
 from rest_framework import viewsets
+from common.permissions import IsOwnerOrReadOnly
 from .models import UserProfile
 from .serializers import UserProfileSerializer
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (
+        permissions.IsAuthenticatedOrReadOnly,
+        IsOwnerOrReadOnly,
+    )
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
