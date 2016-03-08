6576dcc46a2c9266e58c3f2d8a74aaeb8bc5803c
Javier Adriel <reivaj0705@gmail.com>
2016-03-08 01:37:29 +0000

    Add permission to let only aunthenticated users modify users

--- surveys_rest_api/accounts/views.py
+++ surveys_rest_api/accounts/views.py
@@ -1,8 +1,10 @@
+from rest_framework import permissions
 from rest_framework import viewsets
 from .models import UserProfile
 from .serializers import UserProfileSerializer
 
 
 class UserProfileViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
