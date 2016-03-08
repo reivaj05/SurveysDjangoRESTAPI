686ed9496d99f0c5ee36805823af3c6e70e2cb69
Javier Adriel <reivaj0705@gmail.com>
2016-03-08 02:17:47 +0000

    Add surveys urls to router

--- surveys_rest_api/api/urls.py
+++ surveys_rest_api/api/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url, include
 from rest_framework import routers
 from accounts.views import UserProfileViewSet
+from surveys.views import SurveyViewSet
 
 router = routers.DefaultRouter()
 router.register(r'accounts', UserProfileViewSet)
+router.register(r'surveys', SurveyViewSet)
 
 urlpatterns = [
-    url(r'^surveys/', include('surveys.urls')),
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
