1f1e9626642fef2d2d34afaf3b7c4bb81a960e01
Javier Adriel <reivaj0705@gmail.com>
2016-03-08 23:44:36 +0000

    Refactor of code for creating survey

--- surveys_rest_api/surveys/serializers.py
+++ surveys_rest_api/surveys/serializers.py
@@ -60,29 +60,38 @@ class SurveySerializer(serializers.ModelSerializer):
         read_only_fields = ('created_at', 'updated_at', 'user')
 
     def create(self, validated_data):
-        sections_data = validated_data.pop('sections')
-        survey_created = Survey.objects.create(**validated_data)
-        for section in sections_data:
-            questions_data = section.pop('questions')
-            section_created = Section.objects.create(
-                survey=survey_created,
-                **section
-            )
-            for question in questions_data:
-                options_data = question.pop('options')
-                question_type = question.pop('question_type')
-                question_created = Question.objects.create(
-                    section=section_created,
-                    question_type=question_type,
-                    **question
-                )
-                for option in options_data:
-                    Option.objects.create(question=question_created, **option)
+        return create_survey(validated_data)
 
-        # self.create_data(sections_data, Section, 'survey=fk', survey)
-        return survey_created
 
-    def create_data(self, data, model, assignment, fk):
-        print eval(assignment)
-        for element in data:
-            model.objects.create(eval(assignment), **element)
+def create_options(options_data, question_created):
+    for option in options_data:
+        Option.objects.create(question=question_created, **option)
+
+
+def create_questions(questions_data, section_created):
+    for question in questions_data:
+        options_data = question.pop('options')
+        question_type = question.pop('question_type')
+        question_created = Question.objects.create(
+            section=section_created,
+            question_type=question_type,
+            **question
+        )
+        create_options(options_data, question_created)
+
+
+def create_sections(sections_data, survey_created):
+    for section in sections_data:
+        questions_data = section.pop('questions')
+        section_created = Section.objects.create(
+            survey=survey_created,
+            **section
+        )
+        create_questions(questions_data, section_created)
+
+
+def create_survey(validated_data):
+    sections_data = validated_data.pop('sections')
+    survey_created = Survey.objects.create(**validated_data)
+    create_sections(sections_data, survey_created)
+    return survey_created
